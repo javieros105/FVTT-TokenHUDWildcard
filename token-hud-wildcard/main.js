c6096fc42d01c4584170c789aa7408b0d91f6794
Javier Romero <jromeroschmidt@gmail.com>
2020-07-31 01:31:30 -0400

    Refactoring of the code and changes in style
    + Refactored code so it doesn't destroy the panel when clicking the
    button but instead it changes visibility
    + Made style be mofe in line with the default HUD
    + Added personalized HUD button icon
    + When clicked detects if status effects is open or not and closes it,
    the image panel is also closed when clicking another HUD button

--- token-hud-wildcard/main.js
+++ token-hud-wildcard/main.js
@@ -6,51 +6,50 @@ Hooks.on("renderTokenHUD", async (hud, html, token) => {
 		return
 	}
 
-	let openButton = document.createElement("div");
-
-	openButton.classList.add("control-icon");
-	openButton.classList.add("image-selector");
-
-	openButton.innerHTML = `<i class="fas fa-chevron-right"></i>`
-	openButton.title = `${game.i18n.localize("THWildcard.Title")}`
-
-	$(openButton).click((event) => {
-		const buttonFind = html.find('.control-icon.image-selector')
-		const active = buttonFind[0].classList.contains('active')
-		if (!active) {
-			let imNames = `
-				<div class="image-swapper-selector-wrap">
+	html.find("div.right")
+		.append(`
+			<div class="control-icon thwildcard-selector" >
+				<img src="modules/token-hud-wildcard/img/token-icon.svg" width="36" height="36" title="${game.i18n.localize("THWildcard.Title")}" />
+				<div class="thwildcard-selector-wrap">
 					${
 						images.map((im, index) => {
 							const split = im.split('/')
 							const image = split[split.length - 1]
 							return `
-								<button class="image-swapper-button-select" name="${im}">${image}</button>
+								<button class="thwildcard-button-select control-icon" name="${im}">${image}</button>
 							`
 						}).join('')
 					}
 				</div>
-			`
+			</div>
+		`)
+		.click((event) => {
+			const buttonFind = html.find('.control-icon.thwildcard-selector')
+			const cList = event.target.parentElement.classList
+			const correctButton = cList.contains('thwildcard-selector') 
+			const active = cList.contains('active')
 
-			buttonFind[0].classList.add('active')
-			buttonFind.after(imNames)
+			if (correctButton && !active) {
+				buttonFind[0].classList.add('active')
+				html.find('.thwildcard-selector-wrap')[0].classList.add('active')
 
-			const buttons = html.find(".image-swapper-button-select")
-			buttons.map((button) => {
-				buttons[button].addEventListener('click', function (event) {
-					event.preventDefault()
-					event.stopPropagation()
-					const controlled = canvas.tokens.controlled
-					const index = controlled.findIndex(x => x.data._id === token._id)
-					const tokenToChange = controlled[index]
-					tokenToChange.update({ img: event.target.name })
-				})
-			})
-		} else {
-			buttonFind[0].classList.remove('active')
-			html.find('.image-swapper-selector-wrap')[0].remove()
-		}
+				html.find('.control-icon.effects')[0].classList.remove('active')
+				html.find('.status-effects')[0].classList.remove('active')
+			} else {
+				buttonFind[0].classList.remove('active')
+				html.find('.thwildcard-selector-wrap')[0].classList.remove('active')
+			}
+		});
+
+	const buttons = html.find(".thwildcard-button-select")
+	buttons.map((button) => {
+		buttons[button].addEventListener('click', function (event) {
+			event.preventDefault()
+			event.stopPropagation()
+			const controlled = canvas.tokens.controlled
+			const index = controlled.findIndex(x => x.data._id === token._id)
+			const tokenToChange = controlled[index]
+			tokenToChange.update({ img: event.target.name })
+		})
 	})
-
-	html.find("div.right").append(openButton);
 })
