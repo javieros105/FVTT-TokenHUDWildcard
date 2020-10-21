e0a7a64084362499e29cf284c8ad9b2cd8561079
Javier Romero <jromeroschmidt@gmail.com>
2020-10-21 19:46:45 -0300

    Updates for PF and opacity control
    * Now it works with renderTokenConfig in PF1
    * Added a global setting to allow setting opacity of token image preview
    * Improved localization for english and spanish

--- token-hud-wildcard/main.js
+++ token-hud-wildcard/main.js
@@ -1,123 +1,127 @@
 const tokenHUDWildcard = {
-	registerSettings: () => {
-		game.settings.register('token-hud-wildcard', 'imageDisplay', {
-			name : 'Display as Image?',
-			hint : 'Disable to display wildcard options as a list of their filenames',
-			scope : 'world',
-			config : true,
-			type : Boolean,
-			default : true
-		})
-	}
+    registerSettings: () => {
+        game.settings.register('token-hud-wildcard', 'imageDisplay', {
+            name: game.i18n.format('THWildcard.DisplaySettingName'),
+            hint: game.i18n.format('THWildcard.DisplaySettingHint'),
+            scope: 'world',
+            config: true,
+            type: Boolean,
+            default: true
+        })
+        game.settings.register("token-hud-wildcard", "imageOpacity", {
+            name: game.i18n.format('THWildcard.OpacitySettingName'),
+            hint: game.i18n.format('THWildcard.OpacitySettingHint'),
+            scope: "world",
+            config: true,
+            range: { min: 0, max: 100, step: 1 },
+            type: Number,
+            default: 50
+        })
+    }
 }
 
-Hooks.on('init', ()=> {
+Hooks.on('init', () => {
     tokenHUDWildcard.registerSettings()
 })
 
 Hooks.on('ready', () => {
-	const renderTokenConfig = `renderTokenConfig${game.system.id === 'pf1' ? 'PF' : ''}`
-	Hooks.on(renderTokenConfig, WildcardDefault.render)
-	WildcardDefault._hookPreTokenCreate()
+    const renderTokenConfig = 'renderTokenConfig'
+    Hooks.on(renderTokenConfig, WildcardDefault.render)
+    WildcardDefault._hookPreTokenCreate()
 })
 
 const WildcardDefault = {
-	getDefaultImg: async (token) => {
-		const flag = token.getFlag('token-hud-wildcard', 'default') || ''
-		return flag
-	},
-	_hookPreTokenCreate() {
+    getDefaultImg: async (token) => {
+        const flag = token.getFlag('token-hud-wildcard', 'default') || ''
+        return flag
+    },
+    _hookPreTokenCreate () {
         Hooks.on('preCreateToken', (scene, data, options, userId) => {
-			const actor = game.actors.get(data.actorId);
-			console.log(actor)
-			const defaultValue = data.flags['token-hud-wildcard'] ? data.flags['token-hud-wildcard'].default : ''
-			if (defaultValue !== '' && actor.data.token.randomImg) {
-				setProperty(data, 'img', defaultValue);
-			}
+            const actor = game.actors.get(data.actorId)
+
+            const defaultValue = data.flags['token-hud-wildcard'] ? data.flags['token-hud-wildcard'].default : ''
+            if (defaultValue !== '' && actor.data.token.randomImg) {
+                setProperty(data, 'img', defaultValue)
+            }
         })
     },
-	render: async (config, html) => {
-		const defaultImg = await WildcardDefault.getDefaultImg(config.token)
-		if (config.token.data._id) {
-			return
-		}
-		const imageDataTab = html.find('.tab[data-tab="image"]')
-		const checkBoxWildcard = imageDataTab.find('input[name="randomImg"]')
-		let configField = await renderTemplate('/modules/token-hud-wildcard/templates/configField.html', { defaultImg, available: checkBoxWildcard[0].checked })
+    render: async (config, html) => {
+        const defaultImg = await WildcardDefault.getDefaultImg(config.token)
+        if (config.token.data._id) {
+            return
+        }
+        const imageDataTab = html.find('.tab[data-tab="image"]')
+        const checkBoxWildcard = imageDataTab.find('input[name="randomImg"]')
+        const configField = await renderTemplate('/modules/token-hud-wildcard/templates/configField.html', { defaultImg, available: checkBoxWildcard[0].checked })
 
-		imageDataTab.append(configField)
-		const defaultConfig = imageDataTab.find('.thwildcard-default')
+        imageDataTab.append(configField)
+        const defaultConfig = imageDataTab.find('.thwildcard-default')
 
-		defaultConfig.find('button').click(event => {
-			event.preventDefault()
-			const input = defaultConfig.find('input')[0]
-			console.log(input)
-			const fp = new FilePicker({ current: input.value, field: input })
-    		fp.browse(defaultImg)
-		})
+        defaultConfig.find('button').click(event => {
+            event.preventDefault()
+            const input = defaultConfig.find('input')[0]
 
-		checkBoxWildcard.click(event => {
-			// console.log(event.target.checked)
-			if (event.target.checked) {
-				defaultConfig[0].classList.add('active')
-			} else {
-				defaultConfig[0].classList.remove('active')
-			}
+            const fp = new FilePicker({ current: input.value, field: input })
+            fp.browse(defaultImg)
+        })
 
-		})
-	}
+        checkBoxWildcard.click(event => {
+            if (event.target.checked) {
+                defaultConfig[0].classList.add('active')
+            } else {
+                defaultConfig[0].classList.remove('active')
+            }
+        })
+    }
 }
 
 Hooks.on('renderTokenHUD', async (hud, html, token) => {
-	let actor = game.actors.get(token.actorId)
-	let images = await actor.getTokenImages()
+    const actor = game.actors.get(token.actorId)
+    const images = await actor.getTokenImages()
 
-	if (images.length < 2) {
-		return
-	}
+    if (images.length < 2) {
+        return
+    }
 
-	let imageDisplay = game.settings.get('token-hud-wildcard', 'imageDisplay')
-	let imagesParsed = images.map(im => {
-		const split = im.split('/')
-		return { route: im, name: split[split.length - 1], used: im === token.img }
-	})
+    const imageDisplay = game.settings.get('token-hud-wildcard', 'imageDisplay')
+    const imageOpacity = game.settings.get('token-hud-wildcard', 'imageOpacity')/100
+    const imagesParsed = images.map(im => {
+        const split = im.split('/')
+        return { route: im, name: split[split.length - 1], used: im === token.img }
+    })
 
-	console.log(imagesParsed)
+    const wildcardDisplay = await renderTemplate('/modules/token-hud-wildcard/templates/hud.html', { imagesParsed, imageDisplay, imageOpacity })
 
-	let wildcardDisplay = await renderTemplate('/modules/token-hud-wildcard/templates/hud.html', { imagesParsed, imageDisplay })
+    html.find('div.right')
+        .append(wildcardDisplay)
+        .click((event) => {
+            const buttonFind = html.find('.control-icon.thwildcard-selector')
+            const cList = event.target.parentElement.classList
+            const correctButton = cList.contains('thwildcard-selector')
+            const active = cList.contains('active')
 
-	html.find('div.right')
-		.append(wildcardDisplay)
-		.click((event) => {
-			const buttonFind = html.find('.control-icon.thwildcard-selector')
-			const cList = event.target.parentElement.classList
-			const correctButton = cList.contains('thwildcard-selector')
-			const active = cList.contains('active')
+            if (correctButton && !active) {
+                buttonFind[0].classList.add('active')
+                html.find('.thwildcard-selector-wrap')[0].classList.add('active')
 
-			if (correctButton && !active) {
-				buttonFind[0].classList.add('active')
-				html.find('.thwildcard-selector-wrap')[0].classList.add('active')
+                html.find('.control-icon.effects')[0].classList.remove('active')
+                html.find('.status-effects')[0].classList.remove('active')
+            } else {
+                buttonFind[0].classList.remove('active')
+                html.find('.thwildcard-selector-wrap')[0].classList.remove('active')
+            }
+        })
 
-				html.find('.control-icon.effects')[0].classList.remove('active')
-				html.find('.status-effects')[0].classList.remove('active')
-			} else {
-				buttonFind[0].classList.remove('active')
-				html.find('.thwildcard-selector-wrap')[0].classList.remove('active')
-			}
-		})
+    const buttons = html.find('.thwildcard-button-select')
 
-	const buttons = html.find('.thwildcard-button-select')
-
-	buttons.map((button) => {
-		buttons[button].addEventListener('click', function (event) {
-			event.preventDefault()
-			event.stopPropagation()
-			console.log('Click Button', event)
-			console.log('Token', token)
-			const controlled = canvas.tokens.controlled
-			const index = controlled.findIndex(x => x.data._id === token._id)
-			const tokenToChange = controlled[index]
-			tokenToChange.update({ img: event.target.dataset.name })
-		})
-	})
+    buttons.map((button) => {
+        buttons[button].addEventListener('click', function (event) {
+            event.preventDefault()
+            event.stopPropagation()
+            const controlled = canvas.tokens.controlled
+            const index = controlled.findIndex(x => x.data._id === token._id)
+            const tokenToChange = controlled[index]
+            tokenToChange.update({ img: event.target.dataset.name })
+        })
+    })
 })
